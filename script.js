837ea8af647b897eb568f2e5fe27dc5b2308c840
luoxiang <luox5005@uwec.edu>
2020-03-11 01:21:54 -0500

    new

--- script.js
+++ script.js
@@ -4,17 +4,6 @@ var list = $("list").getElementsByTagName("li");
 var round_r = $("arrow_r");
 var round_l = $("arrow_l");
 var index = 0;
-for (var i = 0; i < list.length; i++) {
-    list[i].index = i;
-    list[i].onclick = function(){
-        for(var j = 0;j<list.length;j++){
-            list[j].className="";
-            image[j].className="";
-        }
-        this.className="list_show";
-        image[this.index].className="image_show";
-    }
-}
 round_r.onclick = function(){
     move(false);
 }
@@ -47,5 +36,5 @@ function move(boolean){
         image[j].className="";
     }
     list[index].className="list_show";
-    image[index].className="image_show";
+    image[index].className="image_show fade";
 }
