c9f577f8fb422380c32fb6f91f784e24d20d5e00
luoxiang <luox5005@uwec.edu>
2020-03-10 22:07:19 -0500

    new

---
+++ scripth.js
@@ -0,0 +1,10 @@
+var speed=15
+demo2.innerHTML=demo1.innerHTML
+function Marquee(){
+    if(demo2.offsetWidth-demo.scrollLeft <=0)
+        demo.scrollLeft-=demo1.offsetWidth
+    else{
+        demo.scrollLeft++
+    }
+}
+var MyMar=setInterval(Marquee,speed)
