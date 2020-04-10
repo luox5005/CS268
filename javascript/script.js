44c869a0f76f4f2d5a8319e6afa92377bd022c3e
Xiang Luo <luox5005@uwec.edu>
2020-04-10 06:44:54 -0500

    new

--- javascript/script.js
+++ javascript/script.js
@@ -1,40 +1,17 @@
-function $(id){return document.getElementById(id);}
-var image = $("image").getElementsByTagName("li");
-var list = $("list").getElementsByTagName("li");
-var round_r = $("arrow_r");
-var round_l = $("arrow_l");
-var index = 0;
-round_r.onclick = function(){
-    move(false);
-}
-round_l.onclick = function(){
-    move(true);
-}
-timer = null;
-        if (timer) {
-            clearInterval(timer);
-            timer = null;
-        }
-        timer = setInterval(autoPlay, 3000);
-        function autoPlay() {
-            index++;
-            if (index >= list.length) {
-                index = 0;
-            }
-            move(false);
-        }
-function move(boolean){
-    for(var i = 0;i<list.length;i++){
-        if(list[i].className=="list_show"){
-            var index = i;
-            break;
-        }
-    }
-    boolean ?(index == 0? index= list.length-1:index--):(index == list.length-1? index= 0:index++);
-    for(var j = 0;j<list.length;j++){
-        list[j].className="";
-        image[j].className="";
-    }
-    list[index].className="list_show";
-    image[index].className="image_show fade";
-}
+const menuButton = document.getElementById('menu-button');
+const navigation = document.getElementById('menu');
+
+menuButton.addEventListener('click', e => {
+  menuButton.classList.toggle('active');
+  navigation.classList.toggle('open');
+});
+
+document.addEventListener('click', () => {
+    menuButton.classList.remove('active');
+    navigation.classList.remove('open');
+  });
+
+  menuButton.addEventListener('click', e => {
+    /* ... */
+    e.stopPropagation();
+  });
